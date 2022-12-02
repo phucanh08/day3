9ab9af66210f1b0964c29d06ebd8cf57f077a51f
ETC\anhlp <anhlp@etc.vn>
2022-12-02 17:14:55 +0700

    xong search

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -64,18 +64,14 @@ public class MyArrayList<E> {
     }
 
     public <A, B> void selectedSort(MyFunction<E, Boolean> function, int l, int r) {
-        if(r < list.size()) {
-            if(r != 0) {
-                for (int i = l; i < r; i++) {
-                    if (function.apply(list.get(i), list.get(r + 1))) {
-                        E e = list.get(r + 1);
-                        list.remove(r + 1);
-                        list.add(r, e);
-                        selectedSort(function, l, r + 1);
-                    }
+        if (r < list.size()) {
+            for (int i = l; i < r; i++) {
+                if (function.apply(list.get(i), list.get(r + 1))) {
+                    E e = list.get(r + 1);
+                    list.remove(r + 1);
+                    list.add(r, e);
+                    selectedSort(function, l, r + 1);
                 }
-            } else {
-
             }
         }
     }
